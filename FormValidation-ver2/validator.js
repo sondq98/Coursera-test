ae48f494da679fafe31021e6bb1d699bc5fdda1d
Sondq <sondq98@gmail.com>
2021-03-21 13:01:48 +0700

    Add form validation version 2

---
+++ FormValidation-ver2/validator.js
@@ -0,0 +1,9 @@
+function Validator(formSelector) {
+    var formElement = document.querySelector('formSelector')
+
+    if (formElement) {
+        
+    } else {
+        alert('formSelector không tốn tại')
+    }
+}
